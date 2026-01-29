b04dfddfe2c32f3bcfb09525d9d050c8527743f9
shivam <shiv10212005@gmail.com>
2026-01-29 12:59:45 +0530

    Hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <main className=''>
+        <Hero />
+      </main>
     </div>
-  )
-}
+  );
+};
 
 export default Home
